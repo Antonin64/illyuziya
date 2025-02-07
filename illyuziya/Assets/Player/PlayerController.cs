dce973ccde3ee6233b52cdd4f9671a73dac5ae3c
DarquesTheo <DarquesTheo@users.noreply.github.com>
2025-02-07 18:21:18 +0100

    Added player (didn't test)

---
+++ illyuziya/Assets/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float speed = 5f;
+
+    private CharacterController characterController;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3  move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        characterController.Move(move * Time.deltaTime * speed);
+    }
+}
